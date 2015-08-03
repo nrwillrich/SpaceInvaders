0f18d78e0ffd3a0a9e60213ce730872721932103
hellwilliam <gbourscheidt@hotmail.com>
2015-08-03 19:00:17 -0300

    Menu

--- SpaceInvaders/World.cs
+++ SpaceInvaders/World.cs
@@ -150,32 +150,34 @@ namespace SpaceInvaders
             }
         }
 
+        //float lengthPlay = 0.0f;
+
         private void MainMenu(GameTime gameTime)
         {
             // https://www.youtube.com/watch?v=axlx3o0codc
             // https://danieltian.wordpress.com/2008/12/24/xna-tutorial-typewriter-text-box-with-proper-word-wrapping-part-3/
 
             String strPlay = "PLAY";
-            float lengthPlay = 0.0f;
 
-            m_spriteBatch.DrawString(m_font, "SCORE< 1 >    HI-SCORE    SCORE< 2 >", new Vector2(60.0f, 75.0F), Color.White);
-            m_spriteBatch.DrawString(m_font, "  " + p1Score.ToString("D4") + "     " + highScore.ToString("D4") + "      0000", new Vector2(60.0f, 95.0F), Color.White);
+            m_spriteBatch.DrawString(m_font, "SCORE< 1 >    HI-SCORE    SCORE< 2 >", new Vector2(70.0f, 75.0F), Color.White);
+            m_spriteBatch.DrawString(m_font, "  " + p1Score.ToString("D4") + "     " + highScore.ToString("D4") + "      0000", new Vector2(70.0f, 95.0F), Color.White);
             
-            m_spriteBatch.DrawString(m_font, strPlay.Substring(0, (int) lengthPlay), new Vector2(90.0f, 135.0F), Color.White);
+            //m_spriteBatch.DrawString(m_font, strPlay.Substring(0, (int) lengthPlay), new Vector2(90.0f, 135.0F), Color.White);
 
-            if ((int ) lengthPlay < strPlay.Length)
-            {
-                lengthPlay = lengthPlay + (float) gameTime.ElapsedGameTime.TotalSeconds;
-            } 
+            //if ((int ) lengthPlay < strPlay.Length)
+            //{
+            //    lengthPlay = lengthPlay + ((float) gameTime.ElapsedGameTime.TotalSeconds * 10);
+            //}
             
+            m_spriteBatch.DrawString(m_font, "PLAY", new Vector2(200.0f, 135.0F), Color.White);
             m_spriteBatch.DrawString(m_font, "SPACE INVADERS", new Vector2(150.0f, 175.0f), Color.White);
-            m_spriteBatch.DrawString(m_font, "   *SCORE ADVANCE TABLE*", new Vector2(150.0f, 225.0f), Color.White);
+            m_spriteBatch.DrawString(m_font, "   *SCORE ADVANCE TABLE*", new Vector2(107.0f, 225.0f), Color.White);
             //m_spriteBatch.DrawString(m_font, "= ? mystery", new Vector2(190.0f, 190.0f), Color.White);
             //m_spriteBatch.DrawString(m_font, "= 30 points", new Vector2(190.0f, 230.0f), Color.White);
             //m_spriteBatch.DrawString(m_font, "= 20 points", new Vector2(190.0f, 270.0f), Color.White);
             //m_spriteBatch.DrawString(m_font, "= 10 points", new Vector2(190.0f, 310.0f), Color.White);
             //m_spriteBatch.DrawString(m_font, "Press Enter to Start", new Vector2(45.0f, 380.0f), Color.White);
-            m_spriteBatch.DrawString(m_font, "   *TAITO CORPORATION*", new Vector2(150.0f, 425.0f), Color.Green);
+            m_spriteBatch.DrawString(m_font, "   *TAITO CORPORATION*", new Vector2(130.0f, 425.0f), Color.Green);
         }
 
         public World()
